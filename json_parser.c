19c80641d23c2d8779a52e3ac761e1cae611bdd0
kewei <kewei.train@gmail.com>
2016-02-22 03:01:58 +0800

    Add more error handling and avoid mem leak

--- json_parser.c
+++ json_parser.c
@@ -42,10 +42,9 @@ int _parse_symbol(FILE *fptr, char *target, char *first_meet)
 	char c;
 	int ret, i;
 
-	ret = -1;
+	ret = -EINVAL;
 	while ((c = getc(fptr)) != EOF) {
 		for (i = 0; i < strlen(target); i++) {
-			//printf("target = %c\n", target[i]);
 			if (c == target[i]) {
 				if (first_meet)
 					*first_meet = c;
@@ -60,7 +59,7 @@ int _parse_symbol(FILE *fptr, char *target, char *first_meet)
 			continue;
 		} else {
 			printf("encounter %c\n", c);
-			ret = -1;
+			ret = -EINVAL;
 			break;
 		}
 	}
@@ -68,22 +67,38 @@ int _parse_symbol(FILE *fptr, char *target, char *first_meet)
 	return ret;
 }
 
+#define ALLOC_UNIT 256
+
 int _parse_string(FILE *fptr, char **string)
 {
 	char c;
 	int ret;
 	char *sptr;
+	long long mem_size, now_len;
 
-	sptr = malloc(sizeof(char) * 256);
+	sptr = malloc(sizeof(char) * ALLOC_UNIT);
+	if (!sptr)
+		return -ENOMEM;
+	mem_size = ALLOC_UNIT;
 	*string = sptr;
+	now_len = 0;
 
-	ret = -1;
+	ret = -EINVAL;
 	while ((c = getc(fptr)) != EOF) {
 		if (c == '"') {
 			ret = 0;
 			break;
 		} else {
 			*sptr++ = c;
+			now_len++;
+			if (now_len == mem_size) {
+				sptr = realloc(*string, mem_size + ALLOC_UNIT);
+				if (!sptr)
+					return -ENOMEM;
+				mem_size += ALLOC_UNIT;
+				*string = sptr;
+				sptr += now_len;
+			}
 		}
 	}
 
@@ -100,7 +115,12 @@ int _parse_integer(FILE *fptr, long long *num)
 		if (0 <= c - '0' && c - '0' <= 9 ) {
 			*num = *num * 10 + (c - '0');
 		} else {
-			break;
+			if (c == ',' || c == ' ' || c == '\n' || c == '\r' || c == '\t')
+				break;
+			else {
+				printf("Error in file pos %ld. Encounter %c\n", ftell(fptr), c);
+				return -EINVAL;
+			}
 		}
 	}
 
@@ -147,12 +167,12 @@ void _print_json(JSON_T *json_obj, long long layer)
 }
 
 #define _PARSE_SYMBOL(fptr, str_ptr, first_meet_ptr) \
-	{ \
+	{\
 		ret = _parse_symbol(fptr, str_ptr, first_meet_ptr); \
-		if (ret < 0) { \
-			printf("Error in %d\n", __LINE__); \
-			return ret; \
-		} \
+		if (ret < 0) {\
+			printf("Error in file pos %ld, code line %d\n", ftell(fptr), __LINE__); \
+				goto error_handle;\
+		}\
 	}
 
 void print_json(JSON_T *json_obj)
@@ -165,13 +185,23 @@ int insert_json_obj(JSON_T *json_obj, char *key, JSON_T *value)
 	KEY_LIST_NODE *node;
 	KEY_STRUCT **keys;
 
+	if (!json_obj)
+		return -ENOMEM;
+
+	if (json_obj->type != JSON)
+		return -EINVAL;
+
 	keys = &(json_obj->keys);
 	if ((*keys) == NULL) {
 		*keys = malloc(sizeof(KEY_STRUCT));
+		if (!(*keys))
+			return -ENOMEM;
 		memset(*keys, 0, sizeof(KEY_STRUCT));
 	}
 
 	node = malloc(sizeof(KEY_LIST_NODE));
+	if (!node)
+		return -ENOMEM;
 	node->mapping.key = key;
 	node->mapping.value = value;
 	node->next = 0;
@@ -193,8 +223,14 @@ int get_json_val(JSON_T *json_obj, char *key, JSON_T **value)
 {
 	KEY_LIST_NODE *now;
 
+	if (!json_obj)
+		return -ENOENT;
+
 	if (json_obj->type != JSON)
 		return -EINVAL;
+
+	if (!(json_obj->keys))
+		return -ENOENT;
 
 	now = json_obj->keys->head;
 	while (now) {
@@ -211,22 +247,32 @@ int get_json_val(JSON_T *json_obj, char *key, JSON_T **value)
 		return -ENOENT;
 }
 
+/* Recursively free all key-value structure, including of itself */
 void free_json_obj(JSON_T *json_obj)
 {
-	KEY_LIST_NODE *now;
+	KEY_LIST_NODE *now, *prev;
+
+	if (!json_obj)
+		return;
 
 	switch (json_obj->type) {
 	case STRING:
-		free(json_obj->str);
+		if (json_obj->str)
+			free(json_obj->str);
+		free(json_obj);
 		break;
 	case INTEGER:
 		break;
 	case JSON:
+		if (!(json_obj->keys))
+			break;
 		now = json_obj->keys->head;
 		while (now) {
 			free(now->mapping.key);
 			free_json_obj(now->mapping.value);
-			now = now->next; 
+			prev = now;
+			now = now->next;
+			free(prev);
 		}
 		free(json_obj->keys);
 		free(json_obj);
@@ -234,6 +280,37 @@ void free_json_obj(JSON_T *json_obj)
 	default:
 		break;
 	}
+
+	return;
+}
+
+/* Recursively free all key-value structure, excluding of itself */
+void free_json_obj_field(JSON_T *json_obj)
+{
+	KEY_LIST_NODE *now, *prev;
+
+	if (!json_obj)
+		return;
+
+	if (json_obj->type == STRING) {
+		free(json_obj->str);
+		return;
+	} else if (json_obj->type == INTEGER) {
+		return;
+	}
+
+	if (!(json_obj->keys))
+		return;
+
+	now = json_obj->keys->head;
+	while (now) {
+		free(now->mapping.key);
+		free_json_obj(now->mapping.value);
+		prev = now;
+		now = now->next;
+		free(prev);
+	}
+	free(json_obj->keys);
 
 	return;
 }
@@ -250,11 +327,14 @@ int parse_json_file(FILE *fptr, JSON_T *json_obj)
 	if (!json_obj)
 		return -ENOMEM;
 
+	key = NULL;
+	tmpstr = NULL;
+	value = NULL;
 	memset(json_obj, 0, sizeof(JSON_T));
 	json_obj->type = JSON;
 	json_obj->keys = NULL;
 
-	 _PARSE_SYMBOL(fptr, "{", NULL);
+	_PARSE_SYMBOL(fptr, "{", NULL);
 
 	while (!feof(fptr)) {
 		_PARSE_SYMBOL(fptr, "\"}", &first_meet);
@@ -263,51 +343,60 @@ int parse_json_file(FILE *fptr, JSON_T *json_obj)
 
 		ret = _parse_string(fptr, &key);
 		if (ret < 0)
-			return ret;
-
-		printf("string = %s : ", key);
+			goto error_handle;
+		//printf("string = %s : ", key);
 
 		_PARSE_SYMBOL(fptr, ":", NULL);
 		_PARSE_SYMBOL(fptr, "\"{[-0123456789", &first_meet);
 		value = malloc(sizeof(JSON_T));
+		if (!value) {
+			ret = -ENOMEM;
+			goto error_handle;
+		}
 		memset(value, 0, sizeof(JSON_T));
 
 		switch (first_meet) {
 		case '\"':
-			value->type = STRING;
 			ret = _parse_string(fptr, &tmpstr);
 			if (ret < 0)
-				return ret;
+				goto error_handle;
+			value->type = STRING;
 			value->str = tmpstr;
-			printf("value = %s\n", tmpstr);
+			//printf("value = %s\n", tmpstr);
 			break;
 		case '{':
 			fseek(fptr, -1, SEEK_CUR);
 			ret = parse_json_file(fptr, value);
 			if (ret < 0)
-				return ret;
+				goto error_handle;
 			break;
 		case '-':
-			value->type = INTEGER;
 			ret = _parse_integer(fptr, &tmpnum);
 			if (ret < 0)
-				return ret;
+				goto error_handle;
+			value->type = INTEGER;
 			value->integer = -tmpnum;
-			printf("value = %lld\n", -tmpnum);
+			//printf("value = %lld\n", -tmpnum);
 			break;
 		default:
 			fseek(fptr, -1, SEEK_CUR);
-			value->type = INTEGER;
 			ret = _parse_integer(fptr, &tmpnum);
 			if (ret < 0)
-				return ret;
+				goto error_handle;
+			value->type = INTEGER;
 			value->integer = tmpnum;
-			printf("value = %lld\n", tmpnum);
+			//printf("value = %lld\n", tmpnum);
 			break;	
 		}
 
 		/* Insert */
-		insert_json_obj(json_obj, key, value);
+		ret = insert_json_obj(json_obj, key, value);
+		if (ret < 0)
+			goto error_handle;
+
+		key = NULL;
+		tmpstr = NULL;
+		value = NULL;
 
 		_PARSE_SYMBOL(fptr, "},", &first_meet);
 		if (first_meet == '}')
@@ -315,8 +404,19 @@ int parse_json_file(FILE *fptr, JSON_T *json_obj)
 	}
 
 	return 0;
+
+error_handle:
+	free_json_obj_field(json_obj);
+	if (key)
+		free(key);
+	if (tmpstr)
+		free(tmpstr);
+	if (value)
+		free(value);
+	return ret;
 }
 
+/* Testing */
 int main()
 {
 	FILE *fptr;
@@ -330,17 +430,20 @@ int main()
 	ret = parse_json_file(fptr, json_obj);
 	printf("ret = %d\n", ret);
 	fclose(fptr);
+	if (ret < 0)
+		return 0;
 
 	printf("num_keys = %d\n", json_obj->keys->num_keys);
 
 	JSON_T *json_value;
 	json_value = NULL;
-	ret = get_json_val(json_obj, "hadha", &json_value);
+	ret = get_json_val(json_obj, "here", &json_value);
 	if (ret < 0)
 		printf("not found\n");
 	else
 		printf("value is %d\n", json_value->integer);
-	print_json(json_obj);
-	free_json_obj(json_obj);
+	//print_json(json_obj);
+	free_json_obj_field(json_obj);
+	free(json_obj);
 	return 0;
 }
